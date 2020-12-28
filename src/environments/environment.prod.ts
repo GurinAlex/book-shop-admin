62e6742bd3dd23011a8b14b9c758871d9eb9d918
GurinAlex <SashaGurinn@gmail.com>
2020-12-28 12:33:13 +0500

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  api_server: 'https://bookpr.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyBQNRtYWcLArF2L03J4e048jizj9U272Tk',
+    authDomain: 'book-shop-c3af3.firebaseapp.com',
+    storageBucket: 'book-shop-c3af3.appspot.com',
+    projectId: 'book-shop-c3af3',
+  }
 };
